017a1a4637151b0fcac4d18d7b607b4fc932941a
Michael Gröning <groening@silpion.de>
2016-03-10 16:49:32 +0100

    more verbous

--- create.sh
+++ create.sh
@@ -3,6 +3,8 @@ NEWEST_BUILD=$(curl http://$CONSUL_HOST:8500/v1/kv/web/newest_build?raw)
 SWARM_MANAGER=$(curl http://$CONSUL_HOST:8500/v1/kv/web/swarm_manager?raw)
 STR=$(curl http://$CONSUL_HOST:8500/v1/kv/web/sample_create_request?raw)
 echo $NEWEST_BUILD
+echo $SWARM_MANAGER
+echo $STR
 #STR=$(<./container.json)
 new=`echo $STR | sed s/NEWEST_BUILD/$NEWEST_BUILD/g`
 echo $new

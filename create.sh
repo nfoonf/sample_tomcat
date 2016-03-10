fa545b3df77119480cd61f22432c49c69f27b416
Michael Gröning <groening@silpion.de>
2016-03-10 16:45:34 +0100

    modified create.sh to not use local files anymore

--- create.sh
+++ create.sh
@@ -1,9 +1,9 @@
 #!/bin/bash          
 NEWEST_BUILD=$(curl http://$CONSUL_HOST:8500/v1/kv/web/newest_build?raw)
 SWARM_MANAGER=$(curl http://$CONSUL_HOST:8500/v1/kv/web/swarm_manager?raw)
-
+STR=$(curl http://$CONSUL_HOST:8500/v1/kv/web/sample_create_request?raw)
 echo $NEWEST_BUILD
-STR=$(<container.json)
+#STR=$(<./container.json)
 new=`echo $STR | sed s/NEWEST_BUILD/$NEWEST_BUILD/g`
 echo $new
 
